894c3220ad13b06b4f2ef7539b1eb7d1a98affe4
Tommy Harris <tomharris@idtdna.com>
2021-02-24 10:23:51 -0600

    Added flow of control project

---
+++ FlowOfControl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FlowOfControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            var a = 2;
+            var b = 2;
+            var r = a + b;
+
+            if(r == 4)
+            {
+                Console.WriteLine("4");
+            }
+        }
+    }
+}
