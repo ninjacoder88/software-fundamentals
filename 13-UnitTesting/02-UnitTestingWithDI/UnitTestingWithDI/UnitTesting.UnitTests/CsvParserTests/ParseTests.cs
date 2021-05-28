4c4657e25f28a08ea0a1260fe3896101a6208d6c
Tommy Harris <tomharris@idtdna.com>
2021-05-28 14:06:35 -0500

    updated

--- 13-UnitTesting/02-UnitTestingWithDI/UnitTestingWithDI/UnitTesting.UnitTests/CsvParserTests/ParseTests.cs
+++ 13-UnitTesting/02-UnitTestingWithDI/UnitTestingWithDI/UnitTesting.UnitTests/CsvParserTests/ParseTests.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Data;
+using System.Linq;
 using AutoFixture;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 using UnitTestingWithDI;
 
@@ -12,22 +16,88 @@ namespace UnitTesting.UnitTests.CsvParserTests
         [SetUp]
         public void SetUp()
         {
-            _file = new FileProxy();
+            _file = Substitute.For<IFile>();
             _fixture = new Fixture();
         }
         
         [Test]
-        public void TestName()
+        public void FileProxyThrowsException_ExceptionSurfaced()
         {
             //SETUP
             var filePath = _fixture.Create<string>();
             var delimiter = "|";
+            _file.ReadAllLines(Arg.Any<string>()).ThrowsForAnyArgs(new DataException("OH NO"));
+            
+            var SUT = new CsvParser(_file);
+
+            //TEST
+            TestDelegate testDelegate = () => SUT.Parse(filePath, delimiter);
+
+            //VALIDATE
+            var ex = Assert.Throws<DataException>(testDelegate);
+            Assert.That(ex.Message.Contains("OH NO"), Is.True);
+        }
+
+        [Test]
+        public void FileProxyReturnsNull_ThrowsException()
+        {
+            //SETUP
+            var filePath = _fixture.Create<string>();
+            var delimiter = "|";
+            _file.ReadAllLines(Arg.Any<string>()).ReturnsNullForAnyArgs();
+
+            var SUT = new CsvParser(_file);
+
+            //TEST
+            TestDelegate testDelegate = () => SUT.Parse(filePath, delimiter);
+
+            //VALIDATE
+            var ex = Assert.Throws<Exception>(testDelegate);
+            Assert.That(ex.Message.Contains("No file lines exists"), Is.True);
+        }
+
+        [Test]
+        public void BreakingTest()
+        {
+            //SETUP
+            var filePath = _fixture.Create<string>();
+            var delimiter = "|";
+            _file.ReadAllLines(Arg.Any<string>())
+                 .ReturnsForAnyArgs(new string[] { "100|1000", "200|A" });
+
             var SUT = new CsvParser(_file);
 
             //TEST
             var results = SUT.Parse(filePath, delimiter);
 
             //VALIDATE
+            var list = results.ToList();
+            Assert.That(list.Count, Is.EqualTo(2));
+            var firstResult = list[0];
+            Assert.That(firstResult.MfgItemId, Is.EqualTo(100));
+            Assert.That(firstResult.ReferenceNumber, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void CriticalPath()
+        {
+            //SETUP
+            var filePath = _fixture.Create<string>();
+            var delimiter = "|";
+            _file.ReadAllLines(Arg.Any<string>())
+                 .ReturnsForAnyArgs(new string[]{"100|1000","200|2000"});
+
+            var SUT = new CsvParser(_file);
+
+            //TEST
+            var results = SUT.Parse(filePath, delimiter);
+
+            //VALIDATE
+            var list = results.ToList();
+            Assert.That(list.Count, Is.EqualTo(2));
+            var firstResult = list[0];
+            Assert.That(firstResult.MfgItemId, Is.EqualTo(100));
+            Assert.That(firstResult.ReferenceNumber, Is.EqualTo(1000));
         }
 
         private IFile _file;
--- 13-UnitTesting/02-UnitTestingWithDI/UnitTestingWithDI/UnitTesting.UnitTests/ProcessorTests/ProcessTests.cs
+++ 13-UnitTesting/02-UnitTestingWithDI/UnitTestingWithDI/UnitTesting.UnitTests/ProcessorTests/ProcessTests.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture;
+﻿using System.Collections.Generic;
+using AutoFixture;
 using NSubstitute;
 using NUnit.Framework;
 using UnitTestingWithDI;
@@ -18,17 +19,29 @@ namespace UnitTesting.UnitTests.ProcessorTests
         }
         
         [Test]
-        public void TestName()
+        public void CriticalPath()
         {
             //SETUP
             var filePath = _fixture.Create<string>();
             var delimiter = _fixture.Create<string>();
+            _csvParser.Parse(filePath, delimiter).Returns(new List<CsvMfgItem>()
+                                                              {
+                                                                  new CsvMfgItem(){MfgItemId = 100, ReferenceNumber = 1000},
+                                                                  new CsvMfgItem(){MfgItemId = 200, ReferenceNumber = 2000},
+                                                              });
+            _repository.Load("100,200").Returns(new List<MfgItemBarcode>
+                                                    {
+                                                        new MfgItemBarcode() {MfgItemId = 100, Barcode = 111111},
+                                                        new MfgItemBarcode() {MfgItemId = 200, Barcode = 222222},
+                                                    });
             var SUT = new Processor(_repository, _outputProxy, _csvParser);
 
             //TEST
             SUT.Process(filePath, delimiter);
             
             //VALIDATE
+            _outputProxy.Received(1).WriteLine("100,1000,111111");
+            _outputProxy.Received(1).WriteLine("200,2000,222222");
         }
 
         private IRepository _repository;
